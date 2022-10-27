dbffcfc5fef4836361defb44e211bbbdcd194816
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-27 10:31:47 +0530

    calling input function

---
+++ index.js
@@ -0,0 +1,6 @@
+const initialPrice =  document.querySelector('#initial-price');
+const currentPrice = document.querySelector('#current-price');
+const totalStocksQuantity = document.querySelector('#total-stocks');
+const checkButton = document.querySelector('#btn');
+const outputArea = document.querySelector('#output');
+
